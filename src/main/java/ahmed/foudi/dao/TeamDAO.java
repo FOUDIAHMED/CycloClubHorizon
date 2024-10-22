21363ce3b61fec1853b1355d0f452bc55f739a6f
ahmed <foudiahmed82@gmail.com>
2024-10-22 01:23:12 +0100

    adding team dao and interface

---
+++ src/main/java/ahmed/foudi/dao/TeamDAO.java
@@ -0,0 +1,12 @@
+package ahmed.foudi.dao;
+
+import ahmed.foudi.dao.interfaces.TeamInterface;
+import ahmed.foudi.entities.Team;
+import ahmed.foudi.utils.AbstractHibernateDao;
+import org.hibernate.SessionFactory;
+
+public class TeamDAO extends AbstractHibernateDao<Team> implements TeamInterface {
+    public TeamDAO(SessionFactory sessionFactory) {
+        super(Team.class ,sessionFactory);
+    }
+}
