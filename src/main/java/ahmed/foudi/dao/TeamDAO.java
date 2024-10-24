a26e61c149c8ed2fed439b5d11238165df2c6af6
ahmed <foudiahmed82@gmail.com>
2024-10-24 11:45:42 +0100

    add webapplication initializer

--- src/main/java/ahmed/foudi/dao/TeamDAO.java
+++ src/main/java/ahmed/foudi/dao/TeamDAO.java
@@ -4,13 +4,9 @@ import ahmed.foudi.dao.interfaces.TeamInterface;
 import ahmed.foudi.entities.Team;
 import ahmed.foudi.utils.AbstractHibernateDao;
 import org.hibernate.SessionFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TeamDAO extends AbstractHibernateDao<Team> implements TeamInterface {
     public TeamDAO(SessionFactory sessionFactory) {
         super(Team.class ,sessionFactory);
     }
-
-
 }
---
+++ src/main/java/ahmed/foudi/utils/MyWebApplicationInitializer.java
@@ -0,0 +1,12 @@
+package ahmed.foudi.utils;
+
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(jakarta.servlet.ServletContext servletContext) throws ServletException {
+
+    }
+}
