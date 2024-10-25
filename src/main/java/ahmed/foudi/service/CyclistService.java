ce5fda17e9aadd52860f50c3abab9b0df0daaa2c
ahmed <foudiahmed82@gmail.com>
2024-10-25 10:29:11 +0100

    add some methods in CyclistService

--- src/main/java/ahmed/foudi/service/CyclistService.java
+++ src/main/java/ahmed/foudi/service/CyclistService.java
@@ -1,4 +1,60 @@
 package ahmed.foudi.service;
 
-public class CyclistService {
+import ahmed.foudi.dao.CyclistDAO;
+import ahmed.foudi.entities.Cyclist;
+import ahmed.foudi.service.interfaces.CyclistServiceI;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class CyclistService implements CyclistServiceI {
+    private final CyclistDAO cyclistDAO;
+    public CyclistService(CyclistDAO cyclistDAO) {
+        this.cyclistDAO = cyclistDAO;
+    }
+
+
+
+
+
+    @Override
+    public List<Cyclist> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Cyclist findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Cyclist findById(Long id) {
+        return null;
+    }
+
+    public void update(Cyclist cyclist) {
+
+    }
+
+    @Override
+    @Transactional
+    public void save(Cyclist cyclist) {
+        cyclistDAO.create(cyclist);
+
+    }
+
+    @Override
+    public void delete(Cyclist cyclist) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
--- src/main/java/ahmed/foudi/service/interfaces/CyclistServiceI.java
+++ src/main/java/ahmed/foudi/service/interfaces/CyclistServiceI.java
@@ -1,4 +1,19 @@
 package ahmed.foudi.service.interfaces;
 
+import ahmed.foudi.entities.Cyclist;
+
+import java.util.List;
+
 public interface CyclistServiceI {
+
+    public List<Cyclist> findAll();
+    public Cyclist findById(int id);
+
+    Cyclist findById(Long id);
+
+    public void save(Cyclist cyclist);
+    public void delete(Cyclist cyclist);
+    public  void delete(long id);
+
+    void delete(Long id);
 }
