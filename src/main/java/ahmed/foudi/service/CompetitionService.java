f49248377a720a14f7a629ed7215e284e2f30885
ahmed <foudiahmed82@gmail.com>
2024-10-25 10:27:59 +0100

    add some methods in competitionservice

--- src/main/java/ahmed/foudi/service/CompetitionService.java
+++ src/main/java/ahmed/foudi/service/CompetitionService.java
@@ -1,4 +1,60 @@
 package ahmed.foudi.service;
 
-public class CompetitionService {
+import ahmed.foudi.dao.CompetitionDAO;
+import ahmed.foudi.entities.Competition;
+import ahmed.foudi.service.interfaces.CompetitionServiceI;
+
+import java.util.List;
+
+public class CompetitionService implements CompetitionServiceI {
+
+    private final CompetitionDAO competitionDAO;
+    public CompetitionService(CompetitionDAO competitionDAO) {
+
+        this.competitionDAO = competitionDAO;
+    }
+
+
+
+
+
+    @Override
+    public List<Competition> findAll() {
+        return competitionDAO.findAll();
+    }
+
+    @Override
+    public Competition findById(Long id) {
+
+        return competitionDAO.findOne(id);
+    }
+
+    @Override
+    public void update(Competition competition) {
+        competitionDAO.update(competition);
+    }
+
+
+
+
+
+    @Override
+    public void save(Competition competition) {
+        competitionDAO.create(competition);
+
+    }
+
+    @Override
+    public void delete(Competition team) {
+        competitionDAO.delete(team);
+
+    }
+
+
+
+    @Override
+    public void delete(Long id) {
+        competitionDAO.deleteById(id);
+
+    }
 }
--- src/main/java/ahmed/foudi/service/interfaces/CompetitionServiceI.java
+++ src/main/java/ahmed/foudi/service/interfaces/CompetitionServiceI.java
@@ -1,4 +1,23 @@
 package ahmed.foudi.service.interfaces;
 
+import ahmed.foudi.entities.Competition;
+
+import java.util.List;
+
 public interface CompetitionServiceI {
+
+
+    List<Competition> findAll();
+
+
+
+    void update(Competition competition);
+
+    Competition findById(Long id);
+
+    void delete(Competition competition);
+
+    void save(Competition competition);
+
+    void delete(Long id);
 }
