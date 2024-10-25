869f932427b35d023c340b77442fdbb7d2f101a3
ahmed <foudiahmed82@gmail.com>
2024-10-25 11:19:04 +0100

    update stage service

--- src/main/java/ahmed/foudi/service/StageService.java
+++ src/main/java/ahmed/foudi/service/StageService.java
@@ -1,4 +1,46 @@
 package ahmed.foudi.service;
 
-public class StageService {
+import ahmed.foudi.dao.StageDAO;
+import ahmed.foudi.entities.Stage;
+import ahmed.foudi.service.interfaces.StageServiceI;
+
+import java.util.List;
+
+public class StageService implements StageServiceI {
+    private final StageDAO stageDAO;
+    public StageService(StageDAO stageDAO) {
+        this.stageDAO = stageDAO;
+    }
+    @Override
+    public Stage findById(int id) {
+        return stageDAO.findOne(id);
+    }
+
+    @Override
+    public List<Stage> findAll() {
+        return stageDAO.findAll();
+    }
+
+    @Override
+    public void save(Stage stage) {
+        stageDAO.create(stage);
+
+    }
+
+    @Override
+    public void delete(Stage stage) {
+        stageDAO.delete(stage);
+    }
+
+    @Override
+    public void update(Stage stage) {
+        stageDAO.update(stage);
+
+    }
+
+    @Override
+    public void delete(int id) {
+        stageDAO.deleteById(id);
+
+    }
 }
--- src/main/java/ahmed/foudi/service/interfaces/StageServiceI.java
+++ src/main/java/ahmed/foudi/service/interfaces/StageServiceI.java
@@ -1,4 +1,14 @@
 package ahmed.foudi.service.interfaces;
 
+import ahmed.foudi.entities.Stage;
+
+import java.util.List;
+
 public interface StageServiceI {
+    Stage findById(int id);
+    List<Stage> findAll();
+    void save(Stage stage);
+    void delete(Stage stage);
+    void update(Stage stage);
+    void delete(int id);
 }
